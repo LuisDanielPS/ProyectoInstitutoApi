adc3d4c068cc0b4d4d24570d8f4e33870b2be47b
jaguilar97 <aguilar.garita97@gmail.com>
2023-12-03 13:21:19 -0600

    Mantenimiento de matrículas

---
+++ API/API/ProyectoAPI/ProyectoAPI/Controllers/MatriculaController.cs
@@ -0,0 +1,252 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ProyectoAPI.Entities;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+
+namespace ProyectoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatriculaController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        private readonly IUtilitarios _utilitarios;
+        private string _connection;
+
+        public MatriculaController(IConfiguration configuration, IUtilitarios utilitarios)
+        {
+            _configuration = configuration;
+            _connection = _configuration.GetConnectionString("DefaultConnection");
+            _utilitarios = utilitarios;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ConsultarCursos")]
+        public IActionResult ConsultarCursos()
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<SelectListItem>("ConsultarCursos",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ConsultarModalidades")]
+        public IActionResult ConsultarModalidades()
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<SelectListItem>("ConsultarModalidades",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ConsultarNiveles")]
+        public IActionResult ConsultarNiveles()
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<SelectListItem>("ConsultarNiveles",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ConsultarHorarios")]
+        public IActionResult ConsultarHorarios()
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<SelectListItem>("ConsultarHorarios",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ConsultarUsuariosPorRol")]
+        public IActionResult ConsultarUsuariosPorRol()
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<SelectListItem>("ConsultarUsuariosPorRol",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("RegistrarMatricula")]
+        public IActionResult RegistrarMatricula(MatriculaEnt entidad)
+        {
+            try
+            {
+                var verificacionMatricula = VerificarMatricula(entidad.IdUsuario);
+                if (!verificacionMatricula)
+                {
+                    return Ok(150);
+                }
+
+
+                using (var context = new SqlConnection(_connection))
+                {
+                    entidad.IdEstatus = 1;
+
+                    var datos = context.Execute("InsertarMatricula",
+                        new
+                        {
+                            entidad.IdUsuario,
+                            entidad.IdCurso,
+                            entidad.IdModalidad,
+                            entidad.IdNivel,
+                            entidad.IdHorario,
+                            entidad.Monto,
+                            entidad.Comentario
+                        },
+                        commandType: CommandType.StoredProcedure);
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("VerificarMatricula")]
+        public bool VerificarMatricula(long IdUsuario)
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var result = context.ExecuteScalar<int>("VerificarMatricula", new { IdUsuario }, commandType: CommandType.StoredProcedure);
+
+                    if (result == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ConsultarClientes")]
+        public IActionResult ConsultarClientes()
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<UsuarioEnt>("ConsultarClientes",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        [Route("ConsultarUsuariosMatriculados")]
+        public IActionResult ConsultarUsuariosMatriculados()
+        {
+            try
+            {
+                using (var context = new SqlConnection(_connection))
+                {
+                    var datos = context.Query<UsuariosMatriculadosEnt>("ConsultarUsuariosMatriculados",
+                        new { },
+                        commandType: CommandType.StoredProcedure).ToList();
+
+                    return Ok(datos);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ API/API/ProyectoAPI/ProyectoAPI/Entities/UsuariosMatriculadosEnt.cs
@@ -0,0 +1,17 @@
+﻿namespace ProyectoAPI.Entities
+{
+    public class UsuariosMatriculadosEnt
+    {
+        public string Usuario { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido1 { get; set; } = string.Empty;
+        public string Apellido2 { get; set; } = string.Empty;
+        public string Cedula { get; set; } = string.Empty;
+        public DateTime FechaMatricula { get; set; }
+        public DateTime DiaPago { get; set; }
+        public string Curso { get; set; } = string.Empty;
+        public string Modalidad { get; set; } = string.Empty;
+        public string Nivel { get; set; } = string.Empty;
+        public string Horario { get; set; } = string.Empty;
+    }
+}
