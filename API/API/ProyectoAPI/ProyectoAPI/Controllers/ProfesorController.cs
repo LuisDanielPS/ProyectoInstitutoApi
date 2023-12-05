31ace389fa1ec259eb0d1496fbfd1955ac3cf3df
Christopher Cruz <78170426+chriscr07@users.noreply.github.com>
2023-12-04 20:02:35 -0600

    Controlador y lógica de negocio para la gestión de profesores

---
+++ API/API/ProyectoAPI/ProyectoAPI/Controllers/ProfesorController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoAPI.Entities;
+using System;
+using System.Data.SqlClient;
+using System.Data;
+using Dapper;
+using System.Linq;
+
+namespace ProyectoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfesorController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IUtilitarios _utilitarios;
+        private string _connection;
+
+        public ProfesorController(IConfiguration configuration, IUtilitarios utilitarios)
+        {
+            _configuration = configuration;
+            _connection = _configuration.GetConnectionString("DefaultConnection");
+            _utilitarios = utilitarios;
+        }
+
+        // POST: api/Profesor/RegistrarProfesor
+        [HttpPost]
+        [Route("RegistrarProfesor")]
+        public IActionResult PostProfesor([FromBody] ProfesorEnt profesor)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connection))
+                {
+                    var parameters = new DynamicParameters(profesor);
+                    var result = connection.Execute("sp_InsertarProfesor", parameters, commandType: CommandType.StoredProcedure);
+                    return result > 0 ? Ok(result) : NotFound("Profesor no se pudo insertar.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET: api/Profesor/ListarProfesores
+        [HttpGet]
+        [Route("ListarProfesores")]
+        public IActionResult GetProfesores()
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connection))
+                {
+                    var profesores = connection.Query<ProfesorEnt>("sp_ListarProfesores", commandType: CommandType.StoredProcedure).ToList();
+                    return Ok(profesores);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // PUT: api/Profesor/ActualizarProfesor/5
+        [HttpPut]
+        [Route("ActualizarProfesor/{id}")]
+        public IActionResult PutProfesor(long id, [FromBody] ProfesorEnt profesor)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connection))
+                {
+                    var parameters = new DynamicParameters();
+                    parameters.Add("IdProfesor", id);
+                    parameters.AddDynamicParams(profesor);
+                    var result = connection.Execute("sp_ActualizarProfesor", parameters, commandType: CommandType.StoredProcedure);
+                    return result > 0 ? Ok(result) : NotFound("Profesor no encontrado o no se pudo actualizar.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // DELETE: api/Profesor/DesactivarProfesor/5
+        [HttpDelete]
+        [Route("DesactivarProfesor/{id}")]
+        public IActionResult DeleteProfesor(long id)
+        {
+            try
+            {
+                using (var connection = new SqlConnection(_connection))
+                {
+                    var parameters = new DynamicParameters();
+                    parameters.Add("IdProfesor", id);
+                    var result = connection.Execute("sp_DesactivarProfesor", parameters, commandType: CommandType.StoredProcedure);
+                    return result > 0 ? Ok(result) : NotFound("Profesor no encontrado o no se pudo desactivar.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ API/API/ProyectoAPI/ProyectoAPI/Entities/ProfesorEnt.cs
@@ -0,0 +1,18 @@
+﻿namespace ProyectoAPI.Entities
+{
+    public class ProfesorEnt
+    {
+        public long IdProfesor { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public string Correo { get; set; }
+        public string Telefono { get; set; }
+        public string Especializacion { get; set; }
+        public bool Activo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+    }
+
+}
