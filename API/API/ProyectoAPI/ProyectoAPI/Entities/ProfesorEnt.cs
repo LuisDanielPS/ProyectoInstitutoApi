c6462bd91da421d911b8b23a639d21a03c3dfc07
Christopher Cruz <78170426+chriscr07@users.noreply.github.com>
2023-12-04 20:05:53 -0600

    Stored Procedures 04 12 2023 profesores

--- API/API/ProyectoAPI/ProyectoAPI/Entities/ProfesorEnt.cs
+++ API/API/ProyectoAPI/ProyectoAPI/Entities/ProfesorEnt.cs
@@ -3,13 +3,13 @@
     public class ProfesorEnt
     {
         public long IdProfesor { get; set; }
-        public string Cedula { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido1 { get; set; }
-        public string Apellido2 { get; set; }
-        public string Correo { get; set; }
-        public string Telefono { get; set; }
-        public string Especializacion { get; set; }
+        public string Cedula { get; set; } = string.Empty;
+        public string Nombre { get; set; } = string.Empty;
+        public string Apellido1 { get; set; } = string.Empty;
+        public string Apellido2 { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Especializacion { get; set; } = string.Empty;
         public bool Activo { get; set; }
         public DateTime FechaCreacion { get; set; }
         public DateTime? FechaModificacion { get; set; }
